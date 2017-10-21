c3ac0f319f5f91a5664405a6fae7df89bded05b1
“temul” <haiwangnet@hotmail.com>
2017-10-21 21:47:38 +0800

    fix file format bug

--- attnmt_train.sh
+++ attnmt_train.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-# shell to start up xvr attention nmt train process
+# shell to start up xvr nmt train process
 python -m nmt.nmt \
-    --attention=scaled_luong \ 
-    --src=en --tgt=zh \
-    --vocab_prefix=/data/nlp/challenger.ai/xvr_data/vocab \
-    --train_prefix=/data/nlp/challenger.ai/xvr_data/aimt.train \
-    --dev_prefix=/data/nlp/challenger.ai/xvr_data/aimt.validation \
-    --test_prefix=/data/nlp/challenger.ai/xvr_data/aimt.test \
-    --out_dir=/data/nlp/challenger.ai/xvr_attention_model \
-    --num_train_steps=10000 \
+    --attention=scaled_luong \
+    --src=en  --tgt=zh \
+    --vocab_prefix=/home/aimt_data/vocab_nmt \
+    --train_prefix=/home/aimt_data/train \
+    --dev_prefix=/home/aimt_data/valid \
+    --test_prefix=/home/aimt_data/valid \
+    --out_dir=/home/aimt_model/attnmt_1020 \
+    --num_train_steps=800000 \
     --steps_per_stats=100 \
-    --num_layers=2 \
+    --num_layers=4 \
     --num_units=128 \
     --dropout=0.2 \
     --metrics=bleu    
